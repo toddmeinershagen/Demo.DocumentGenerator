318eeb1c4b0734bb2f7d7148f0118300269bb9ff
Todd Meinershagen <todd@meinershagen.net>
2014-02-10 09:29:53 -0600

    Adding a missing file for template generation.

---
+++ Demo.DocumentGenerator.Core.UnitTests/Procedure.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo.DocumentGenerator.Core.UnitTests
+{
+    class Procedure
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}
